7fc289dc8bbd3660218ff753faed9d492d55b621
Gennaro Palmigiano <gennaro.palmigiano@outlook.com>
2023-06-06 16:22:22 +0200

    Modificata Grafica Register e Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,26 @@
-<x-layout>
-<div class="container-fluid mt-5">
-
-    <form method="POST" action="{{route('login')}}">
-        @csrf
-        <div class="mb-3">
-            <label for="exampleEmail" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleEmail" name="email">
+<x-layout namePage="Login">
+    
+    <div class="container-fluid mt-5 pt-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-4">
+                <div class="card-user p-5">
+                    <h1 class="display-6 text-center pb-5">Accedi</h1>
+                    <form method="POST" action="{{route('login')}}">
+                        @csrf
+                        <div class="form-floating mb-3">
+                            <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+                            <label for="inputEmail" class="form-label">Email</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="InputPassword" name="password" placeholder="Password">
+                            <label for="InputPassword" class="form-label">Password</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Entra</button>
+                    </form>
+                    
+                </div>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="InputPassword" class="form-label">Password</label>
-            <input type="password" class="form-control" id="InputPassword" name="password">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+    </div>
+    
 </x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,23 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $namepage ?? 'Presto.it' }}</title>
-    @livewireStyles
+    <title>Presto.it • {{ $namePage ?? 'Home' }}</title>
+    {{-- Icon Font Awesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- Livewire --}}
+    @livewireStyles
+    {{-- Style --}}
     @vite(['resources/css/app.css'])
 </head>
 
 <body>
-    
+
     <x-navbar />
 
-    {{ $slot }}
+    {{$slot}}
 
     <x-footer />
 
+    {{-- Livewire --}}
     @livewireScripts
+    {{-- Script --}}
     @vite(['resources/js/app.js'])
 </body>
 
