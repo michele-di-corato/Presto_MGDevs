016fdba5c51ea03367f982bd9a5922039fbfe7e5
Marco Mazza <marcomazza2804@gmail.com>
2023-06-06 15:34:47 +0200

    rotte home

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
-        <a class="ms-2 navTitle navbar-brand fs-2" href="#">Navbar</a>
+        <a class="ms-2 navTitle navbar-brand fs-2" href="{{route('home')}}">Presto.it</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="a-nav1 nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="a-nav1 nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
                 </li>
                 @auth
                 <li class="nav-item">
