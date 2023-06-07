91474cf5833fe342dd9331383bf65ee59a47cbab
Michele Di Corato <micheled.46@hotmail.it>
2023-06-07 14:52:39 +0200

    Correzione form

--- resources/views/livewire/create-announce.blade.php
+++ resources/views/livewire/create-announce.blade.php
@@ -32,7 +32,7 @@
         </div>
         <div class="row justify-content-around">
             @foreach ($categories as $category)
-                <div class="form-check checkbox-create col-5 col-md-6">
+                <div class="form-check checkbox-create col-12 col-md-6">
                     <input class="form-check-input mx-3" type="radio" value="{{ $category->id }}"
                         wire:model="category_id" id="{{ $category->id }}">
                     <label class="form-check-label" for="{{ $category->id }}">
@@ -42,7 +42,7 @@
             @endforeach
         </div>
         <div class="d-flex justify-content-center align-items-center">
-            <button type="submit" class="btn btn-create mt-3 fs-5">Submit</button>
+            <button type="submit" class="btn btn-create mt-3 fs-5">Invia</button>
             <span class="span-icon"><i class="mt-3 mx-3 bi-arrow bi bi-arrow-left-square"></i></span>
         </div>
 </form>
