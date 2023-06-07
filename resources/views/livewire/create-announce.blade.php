235639b277b7e297406879910905528701919598
Michele Di Corato <micheled.46@hotmail.it>
2023-06-07 11:57:17 +0200

    Fix radio buttons

--- resources/views/livewire/create-announce.blade.php
+++ resources/views/livewire/create-announce.blade.php
@@ -13,19 +13,22 @@
         </div>
         <div class="mb-3">
             <label for="productDescription" class="form-label">Descrizione</label>
-           <textarea class="form-control" id="productDescription" wire:model="description"></textarea>
+            <textarea class="form-control" id="productDescription" wire:model="description"></textarea>
         </div>
-        @foreach($categories as $category)
-        <div class="form-check checkbox-create">
-            <input class="form-check-input mx-3" type="radio" value="{{$category->id}}" wire:model="category_id" id="{{$category->id}}">
-            <label class="form-check-label" for="{{$category->id}}">
-              {{$category->name}}
-            </label>
-          </div>
-          @endforeach
-          <div class="d-flex justify-content-center align-items-center">
-        <button type="submit" class="btn btn-outline-dark btn-create mt-3 fs-5">Submit</button>
-        <span class="span-icon"><i class="mt-3 mx-3 bi-arrow bi bi-arrow-left-square"></i></span>
-          </div>
+        <div class="row justify-content-around">
+            @foreach ($categories as $category)
+                <div class="form-check checkbox-create col-5">
+                    <input class="form-check-input mx-3" type="radio" value="{{ $category->id }}"
+                        wire:model="category_id" id="{{ $category->id }}">
+                    <label class="form-check-label" for="{{ $category->id }}">
+                        {{ $category->name }}
+                    </label>
+                </div>
+            @endforeach
+        </div>
+        <div class="d-flex justify-content-center align-items-center">
+            <button type="submit" class="btn btn-outline-dark btn-create mt-3 fs-5">Submit</button>
+            <span class="span-icon"><i class="mt-3 mx-3 bi-arrow bi bi-arrow-left-square"></i></span>
+        </div>
     </form>
 </div>
