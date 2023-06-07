a03cee1794e54676d2fe13757d9523a7200e7895
Gennaro Palmigiano <gennaro.palmigiano@outlook.com>
2023-06-07 15:03:39 +0200

    Merge

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg fixed-top">
+<nav class="navbar navbar-expand-lg position-fixed w-100">
     
     <div class="container-fluid">
         <a class="ms-2 navTitle navbar-brand fs-2" href="{{route('home')}}">
