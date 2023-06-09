a8e3058096f9810fa862eb664a54acb62ea45ceb
Michele Di Corato <micheled.46@hotmail.it>
2023-06-09 10:25:03 +0200

    Aggiunta errori form di login e registrazione, miglioramento pagina profilo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,27 +1,42 @@
 <x-layout namePage="Login">
-    
+
     <div class="container-fluid pt-5">
         <div class="row justify-content-center">
             <div class="col-12 col-md-4">
                 <div class="card-user p-5">
                     <h1 class="display-6 text-center pb-5">Accedi</h1>
 
-                    <form method="POST" action="{{route('login')}}">
+                    <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                id="inputEmail" name="email" placeholder="Email">
                             <label for="inputEmail" class="form-label">Email</label>
+                            @error('email')
+                                <div class="alert alert-danger">
+                                    <p>{{ $message }}</p>
+                                </div>
+                            @enderror
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="password" class="form-control" id="InputPassword" name="password" placeholder="Password">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                id="InputPassword" name="password" placeholder="Password">
                             <label for="InputPassword" class="form-label">Password</label>
+                            @error('password')
+                                <div class="alert alert-danger">
+                                    <p>{{ $message }}</p>
+                                </div>
+                            @enderror
                         </div>
-                        <button type="submit" class="btn btn-primary">Entra</button>
+                        <div class="mb-3 d-flex justify-content-between align-items-center">
+                            <button type="submit" class="btn btn-primary">Entra</button>
+                            <p>Sei un nuovo utente? <a href="{{ route('register') }}" class="btn">Registrati</a></p>
+                        </div>
                     </form>
-                    
+
                 </div>
             </div>
         </div>
     </div>
-    
+
 </x-layout>
