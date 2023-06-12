a5c0e9a07678fdd53a207daf3a2662d33c602183
Michele Di Corato <micheled.46@hotmail.it>
2023-06-12 11:46:37 +0200

    Formattazione

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -14,39 +14,46 @@ class RevisorController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('isRevisor')->except('becomeRevisor','makeRevisor');
+        $this->middleware('isRevisor')->except('becomeRevisor', 'makeRevisor');
     }
 
 
-    public function index(){
+    public function index()
+    {
         $announce_to_check = Announce::where('is_accepted', null)->first();
-
         return view('revisor.index', compact('announce_to_check'));
-
     }
 
-    public function accept(Announce $announce){
+    public function accept(Announce $announce)
+    {
         $announce->setAccepted(true);
-
         return redirect()->back()->with('confirm', 'Hai accettato l\'annuncio');
     }
 
-    public function reject(Announce $announce){
+    public function reject(Announce $announce)
+    {
         $announce->setAccepted(false);
         return redirect()->back()->with('confirm', 'Hai rifiutato l\'annuncio');
-
     }
 
-    public function becomeRevisor(){
+    public function undoAction(Announce $announce)
+    {
+        $announce->setAccepted(null);
+        return redirect()->back()->with('confirm', 'Hai annullato l\'ultima azione');
+    }
+
+    public function becomeRevisor()
+    {
 
         Mail::to('admin@presto.it')->send(new RevisorMail(Auth::user()));
         return redirect()->back()->with('confirm', 'La candidatura è stata inviata con successo');
     }
 
-    public function makeRevisor(User $user){
+    public function makeRevisor(User $user)
+    {
 
-        Artisan::call('app:makeUserRevisor',['email'=> $user->email]);
+        Artisan::call('app:makeUserRevisor', ['email' => $user->email]);
 
-        return redirect('/')->with('confirm','L\'utente è diventato Revisore');
+        return redirect('/')->with('confirm', 'L\'utente è diventato Revisore');
     }
 }
