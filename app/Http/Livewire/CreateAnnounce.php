aeeedc75d5a9e17c622bf1f43b35daa9c418c20e
Marco Melis <melis.marco98@gmail.com>
2023-06-16 11:51:52 +0200

    Oscuramento Facciale

--- app/Http/Livewire/CreateAnnounce.php
+++ app/Http/Livewire/CreateAnnounce.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use App\Models\Image;
 use Livewire\Component;
 use App\Models\Announce;
@@ -69,9 +70,8 @@ class CreateAnnounce extends Component
             foreach ($this->images as $image) {
                 $newFileName = "announces/{$this->announce->id}";
                 $newImage =  $this->announce->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 300, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([new ResizeImage($newImage->path, 300, 300),new GoogleVisionSafeSearch($newImage->id),new GoogleVisionLabelImage($newImage->id)])->dispatch($newImage->id);
+                
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
